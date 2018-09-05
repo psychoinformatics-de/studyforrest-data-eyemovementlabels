35528c7cb63121cca963adba19ecc841afadd76f
Asim H Dar <asim.h.dar@gmail.com>
2018-09-05 18:29:08 +0200

    Script for data analysis updated for new output. Log axis on main sequence.R amd slope also calculated with log values. Currently counting for data is commented out

--- code/generaldata_structure_withplots.py
+++ code/generaldata_structure_withplots.py
@@ -2,6 +2,7 @@
 #!/usr/bin/python
 #Assigning all the pieces of data per subject into a dictionary with the name of the subject/run as the dictionary key. Inside the dictionary they are saved as dataframes in pandas ready for analysis.
 
+#Run from root
 
 import glob
 import pandas as pd
@@ -22,6 +23,7 @@ list = fulllist     #for both
 #list = fulllist[0:15] # for MRI
 
 dictofsamples = {}
+
 
 for subject in list:
 
@@ -51,43 +53,46 @@ for i in listfor5_1:
 # Extracting lines only that contrain type "SACCADE"
 
 # Make a boolean series if SACCADE or not
-saccades = allsubsrun.label == "SACC"
+saccades = (allsubsrun.label == "SACC") | (allsubsrun.label == "ISAC")
 
 # Seeking out only saccades from the dataset
 saccadesonly = allsubsrun[saccades]
 
 # Plotting
 plt.figure()
-saccadegraph = sns.distplot(saccadesonly.duration,kde=False,norm_hist=True,bins=100)
-#plt.xlim(0, 150)
+# duration * 1000 to convert to ms
+saccadegraph = sns.distplot(saccadesonly.duration*1000,kde=False,norm_hist=True,bins=100)
+plt.xlim(0, 100)
 #plt.ylim(0, 0.07)
-saccadegraph.set(xlabel='Saccade Duration in ms')
+saccadegraph.set(xlabel='Saccade Duration [ms]')
 plt.title('Saccade durations over all subjects')
 plt.draw()
 
 ### Figure A2 All peak velocities plotted against the amplitudes
 plt.figure()
 # Sort out data as above, but now for only subject 24, run 4
-subject24_4 = dictofsamples['sub-24/sub-24_task-movie_run-4_events.tsv'] # currently manually replacing with random subject (9) in MRI
-a2saccades = subject24_4.label == "SACC"
+subject24_4 = dictofsamples['sub-14/sub-24_task-movie_run-4_events.tsv'] # currently manually replacing with random subject (9) in MRI
+a2saccades = (subject24_4.label == "SACC") | (subject24_4.label == "ISAC")
 
 a2saccadesonly = subject24_4[a2saccades] 
 
 # Plotting 
-velampgraph = sns.regplot(a2saccadesonly.amp,a2saccadesonly.peak_vel,fit_reg = False,scatter_kws={"s": 20})
+velampgraph = sns.regplot(a2saccadesonly.amp,a2saccadesonly.peak_vel,fit_reg = False,scatter_kws={"s": 15})
 #velampgraph = sns.scatterplot(x= "amp", y="peak_vel", data = a2saccadesonly) No idea why this doesn't work
 plt.title('Relationship between peak velocity and amplitude (subject 24)')
-velampgraph.set(xlabel='Amplitude', ylabel= 'Peak Velocity')
+velampgraph.set(xlabel='Amplitude', ylabel= 'Peak Velocity',yscale="log",xscale="log")
+
 
 # Inserting a fit for the scatter plot, degree = 1
-p = Polynomial.fit(a2saccadesonly.amp,a2saccadesonly.peak_vel,1)
-plt.plot(*p.linspace())
+#p = Polynomial.fit(a2saccadesonly.amp,a2saccadesonly.peak_vel,1)
+#plt.plot(*p.linspace())
+
 plt.draw()
 
 ### Figure A3 All saccade amplitudes
 
 plt.figure()
-saccadeampgraph = sns.distplot(saccadesonly.amp,kde=False,norm_hist=True,bins=50)
+saccadeampgraph = sns.distplot(saccadesonly.amp,kde=False,norm_hist=True,bins=100)
 saccadeampgraph.set(xlabel='Saccade Amplitude in deg')
 #plt.xlim(0, 16)
 plt.title('Saccade amplitudes over all subjects')
@@ -107,7 +112,7 @@ plt.draw()
 ### Figure A5 All saccade peak_vel * duration (done for Subject 24, run 4)
 
 plt.figure()
-product = a2saccadesonly.duration * a2saccadesonly.peak_vel
+product = a2saccadesonly.duration * 1000 * a2saccadesonly.peak_vel
 productgraph = sns.regplot(a2saccadesonly.amp, product,fit_reg = True,scatter_kws={"s": 1}, line_kws={"color":"r","lw":1})
 productgraph.set(ylabel='Product of peak velocity and duration', xlabel = 'Amplitude in deg')
 #plt.xlim(0, 20)
@@ -138,11 +143,11 @@ def findvalues (segment):
 	
 	"""
 	
-	saccades = segmentlist['segment'+str(segment)].type == "SACC"
+	saccades = (segmentlist['segment'+str(segment)].label == "SACC") | (segmentlist['segment'+str(segment)].label == "ISAC")
 	saccadesonly = segmentlist['segment'+str(segment)][saccades]
 	dur_mean,amp_mean,peak_velmean =  saccadesonly.duration.mean(), saccadesonly.amp.mean(),saccadesonly.peak_vel.mean()
 	dur_std,amp_std,peak_velstd =  saccadesonly.duration.std(), saccadesonly.amp.std(),saccadesonly.peak_vel.std()
-	df = pd.DataFrame ({'#' : ['{0}'.format(segment)],'Duration' : ["{0:.2f} ± {1:.2f}".format(dur_mean,dur_std)], 'Amplitude' : ["{0:.2f} ± {1:.2f}".format(amp_mean,amp_std)], 'Peak Velocity' : ["{0:.2f} ± {1:.2f}".format(peak_velmean,peak_velstd)]})
+	df = pd.DataFrame ({'#' : ['{0}'.format(segment)],'Duration' : ["{0:.2f} ± {1:.2f}".format(dur_mean*1000,dur_std*1000)], 'Amplitude' : ["{0:.2f} ± {1:.2f}".format(amp_mean,amp_std)], 'Peak Velocity' : ["{0:.2f} ± {1:.2f}".format(peak_velmean,peak_velstd)]})
 	
 	return df
 	
@@ -155,7 +160,7 @@ def saccadecount(run_number):
 	
 	for i in dictofsamples.keys():
 		if ("run-"+str(run_number)) in i:
-			saccades = dictofsamples[i].type == "SACC"
+			saccades = (dictofsamples[i].label == "SACC")| (dictofsamples[i].label == "ISAC")
 			saccadecount = np.sum(saccades)
 			list.append(saccadecount)
 			
@@ -186,12 +191,23 @@ def makecountlist(run_number,dictionary):
 def findamppeak (segment):
 	"""
 	For the given segment (input a simple integer) slope and r value of the regression line will be given 
-	
+	For the stats log of each is taken
 	"""
 	
-	peakvel = segmentlist['segment'+str(segment)]['peak_vel']
-	amplitude = segmentlist['segment'+str(segment)]['amplitude']
-	slope, intercept, r_value, p_value, std_err = stats.linregress(amplitude,peakvel)
+	peakvel = segmentlist['segment'+str(segment)].peak_vel
+	amplitude = segmentlist['segment'+str(segment)].amp
+	
+	# in the case erroneous data is found (amp = 0)
+	if min(amplitude) == 0:
+		print "Number of 0 valued amplitudes found :", len(segmentlist['segment'+str(segment)].amp[segmentlist['segment'+str(segment)].amp==0])
+		cleanset = segmentlist['segment'+str(segment)]
+		cleanset = cleanset[cleanset.amp != 0]
+		peakvel =  cleanset.peak_vel
+		amplitude = cleanset.amp	
+	
+	segmentlist['segment'+str(segment)].amp [segmentlist['segment'+str(segment)].amp == 0]
+	
+	slope, intercept, r_value, p_value, std_err = stats.linregress(np.log(amplitude),np.log(peakvel))
 		
 	return slope, r_value
 
@@ -201,10 +217,10 @@ def findvaluesG (segment):
 	
 	"""
 	
-	gliccades = segmentlist['segment'+str(segment)].type == "GLISSADE"
+	gliccades = (segmentlist['segment'+str(segment)].label == "LPSO") | (segmentlist['segment'+str(segment)].label == "HPSO")
 	gliccadesonly = segmentlist['segment'+str(segment)][gliccades]
 	dur_mean,amp_mean =  gliccadesonly.duration.mean(), gliccadesonly.amp.mean()
-	df = pd.DataFrame ({'#' : ['{0}'.format(segment)],'Duration' : ["{0:.2f}".format(dur_mean)], 'Amplitude' : ["{0:.2f}".format(amp_mean)]})
+	df = pd.DataFrame ({'#' : ['{0}'.format(segment)],'Duration' : ["{0:.2f}".format(dur_mean*1000)], 'Amplitude' : ["{0:.2f}".format(amp_mean)]})
 	
 	return df
 
@@ -214,12 +230,12 @@ def findvaluesF (segment):
 	
 	"""
 	
-	fixs = segmentlist['segment'+str(segment)].type == "FIX"
+	fixs = segmentlist['segment'+str(segment)].label == "FIXA"
 	fixsonly = segmentlist['segment'+str(segment)][fixs]
-	dur_mean,velmean =  fixsonly.duration.mean(), fixsonly.avg_velocity.mean()
+	dur_mean,velmean =  fixsonly.duration.mean(), fixsonly.avg_vel.mean()
 	# Find the average number of fixes by dividing the number of FIX only by the total subjects (=30)
 	fix_no = (fixsonly.shape[0])/30 
-	df = pd.DataFrame ({'#' : ['{0}'.format(segment)],'Duration' : ["{0:.2f}".format(dur_mean)], 'Fix Mean' : ["{0}".format(fix_no)], 'Average Velocity' : ["{0:.2f}".format(velmean)]})
+	df = pd.DataFrame ({'#' : ['{0}'.format(segment)],'Duration' : ["{0:.2f}".format(dur_mean*1000)], 'Fix Mean' : ["{0}".format(fix_no)], 'Average Velocity' : ["{0:.2f}".format(velmean)]})
 	
 	return df
 
@@ -230,7 +246,8 @@ segmentlist = {}
 for run in range (1,9):
 	segmentlist["segment{0}".format(run)]= makelist(run,dictofsamples)
 	
-# Run through all segments, finding out values that are needed TO DO: FIND A WAY TO PUT THESE IN A PRINTABLE TABLE FORMAT
+# Run through all segments, finding out values that are needed 
+# TO DO: FIND A WAY TO PUT THESE IN A PRINTABLE TABLE FORMAT
 
 data = pd.DataFrame()
 
@@ -263,30 +280,30 @@ print finaltable5_1
 # Nan lines will be counted as the ones 'removed'. For viewing the majority of the nan members
 # we will use preproc files and make all the data available via a dictionary
 
-dictofsamples_preproc = {}
-
-for subject in list:
-
-	allFiles = glob.glob(subject+"/eyegaze_run*preprocessed.tsv.gz") 
-	frame = pd.DataFrame()
-	
-	for run in allFiles:
-		df = pd.read_csv(run,index_col=None, header=None,delim_whitespace=True)
-		df.columns = ["Velocity","Acceleration","x","y"]
-		dictofsamples_preproc["{0}".format(run)]= df
+#dictofsamples_preproc = {}
+#
+#for subject in list:
+#
+#	allFiles = glob.glob(subject+"/eyegaze_run*preprocessed.tsv.gz") 
+#	frame = pd.DataFrame()
+#	
+#	for run in allFiles:
+#		df = pd.read_csv(run,index_col=None, header=None,delim_whitespace=True)
+#		df.columns = ["Velocity","Acceleration","x","y"]
+#		dictofsamples_preproc["{0}".format(run)]= df
 
 
 # Making two lists, one for sec per min loss and second total percent of loss. Didnt append like
 # before because of the lack of memory to manage a billion (!) line array 
 
-secperminlist=[]
-totallostlist=[]
+#secperminlist=[]
+#totallostlist=[]
  
-for run in range (1,9):
-	
-	secpermin, totallost = makecountlist(run,dictofsamples_preproc)
-	secperminlist.append(secpermin)
-	totallostlist.append(totallost)
+#for run in range (1,9):
+#	
+#	secpermin, totallost = makecountlist(run,dictofsamples_preproc)
+#	secperminlist.append(secpermin)
+#	totallostlist.append(totallost)
 	
 #TODO: make into dataframe (see line 134) TODO: AVERAGE SLOPE INSTEAD OF REGRESSION LINE SLOPE
 
@@ -305,12 +322,16 @@ Table5_2 = pd.DataFrame ()
 	
 for i in range(0,8):
 	
-	new_data = pd.DataFrame ({'#' : ['{0}'.format(segs[i])],'Lost Signal' : ["{0:.1f}".format(secperminlist[i])], 'r sqrd' : ["{0:.3f}".format(rvaluelist[i])], 'Samples Removed (%)' : ["{0:.2f}".format(totallostlist[i])],'Slope' : ['{0:.2f}'.format(slopelist[i])]})
+	#new_data = pd.DataFrame ({'#' : ['{0}'.format(segs[i])],'Lost Signal' : ["{0:.1f}".format(secperminlist[i])], 'r sqrd' : ["{0:.3f}".format(rvaluelist[i])], 'Samples Removed (%)' : ["{0:.2f}".format(totallostlist[i])],'Slope' : ['{0:.2f}'.format(slopelist[i])]})
+	new_data = pd.DataFrame ({'#' : ['{0}'.format(segs[i])], 'r sqrd' : ["{0:.3f}".format(rvaluelist[i])],'Slope' : ['{0:.2f}'.format(slopelist[i])]})
+
 	Table5_2 = Table5_2.append(new_data)
 
 
 
-Table5_2 = Table5_2 [['#', 'Lost Signal','r sqrd', 'Samples Removed (%)', 'Slope']]
+#Table5_2 = Table5_2 [['#', 'Lost Signal','r sqrd', 'Samples Removed (%)', 'Slope']]
+Table5_2 = Table5_2 [['#','r sqrd','Slope']]
+
 Table5_2.index= range(1,9)
 print ("  ")
 print ("Table 5.2")
